d9debf3a7f0e7646e76364b48af765e91095726b
Abo Alhasan <117377154+AboALhasanx@users.noreply.github.com>
2024-08-06 15:00:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 BOT_TOKEN = "" #توكن البوت هنا
-ADMIN_ID = 5664798395
+ADMIN_ID = "" #ايديك هنا 
 
 # Channel ID's
-cs_stg4 = -1002245627976
-cs_stg4_onefile = -1002231449002
-cs_stg4_deleted = -1001790832933
-cs_apps = -1001802771388
+cs_stg4 = 
+cs_stg4_onefile = 
+cs_stg4_deleted = 
+cs_apps = 
 
